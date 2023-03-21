e40a756f98cf64be0b6da6695028b6f7273db755
Canovao <104451710+Canovao@users.noreply.github.com>
2023-03-21 09:29:25 -0300

    Setup

--- Crawler/CrawlerCodes/Setup.py
+++ Crawler/CrawlerCodes/Setup.py
@@ -1,11 +1,23 @@
 import sqlite3
+import os
+
+nome_pasta = "CrawlData"
+caminho_pasta = "Crawler\\" + nome_pasta
+
+try:
+    os.mkdir(caminho_pasta)
+    print("Folder criado com sucesso!")
+except FileExistsError:
+    print("Folder já existe!")
+except Exception as e:
+    print("Erro ao criar folder:", e)
+    exit(-1)
 
 with open('Crawler\\CrawlData\\LinksToCrawl.json', 'w', encoding='latin-1') as jsonFile:
     jsonFile.close()
 
 with open('Crawler\\CrawlData\\StoredLinks.db', 'w', encoding='latin-1') as db:
     db.close()
-
 
 conn = sqlite3.connect('Crawler\\CrawlData\\StoredLinks.db')
 cursor = conn.cursor()
