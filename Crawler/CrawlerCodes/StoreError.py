413d1eea93396d936b8ed04df1a3e87ffa52cc42
Canovao <104451710+Canovao@users.noreply.github.com>
2023-03-21 11:33:15 -0300

    Visual bugfix

--- Crawler/CrawlerCodes/StoreError.py
+++ Crawler/CrawlerCodes/StoreError.py
@@ -7,8 +7,5 @@ today = date.today()
 current_time = now.strftime(r"%H-%M-%S-%f")
 
 def storeError(error: BaseException, message: str, threadNum: int):
-    print(message)
-    print(error)
-    
     with open(f"Crawler\\Errors\\json{today}--{current_time}--Thread-{str(threadNum)}.json", "w", encoding="utf-8") as jsonFile:
         json.dump([str(error.with_traceback(None)), str(error)], jsonFile)
